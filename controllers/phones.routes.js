49f6f52d6babda3017a5904bb1ae356979637812
Ahmed-469 <ahmed3almanoo@gmail.com>
2026-01-20 20:28:31 +0300

    added routes to create a new used phone

--- controllers/phones.routes.js
+++ controllers/phones.routes.js
@@ -1,14 +1,36 @@
 const router = require("express").Router()
 const Phone = require('../models/Phone')
 
+//read all
 router.get('/', async(req,res)=>{
     try{
     const allPhones = await Phone.find();
-    res.render('phones/all-phones.ejs');
-}
+    res.render('phones/all-phones.ejs',{allPhones: allPhones});
+    }
     catch (error) {
         console.log('ERROR:', error);
     }
 })
+
+//create new phone
+router.get('/create', async(req,res)=>{
+    try{
+        res.render('phones/create-phone.ejs')
+    }
+    catch (error) {
+        console.log('ERROR:', error);
+    }
+})
+
+router.post('/', async(req,res) => {
+    try {
+        const createdPhone = await Phone.create(req.body);
+        res.redirect('/used-phones');
+    }
+    catch (error) {
+        console.log('ERROR:', error);
+    }
+});
+
 
 module.exports = router;
