2de45dc3347f5c8f3b2f8a12eea7491ae581592b
Ahmed-469 <ahmed3almanoo@gmail.com>
2026-01-22 12:06:35 +0300

    Added delete button and added console logging for all routes

--- controllers/phones.routes.js
+++ controllers/phones.routes.js
@@ -6,6 +6,7 @@ router.get('/', async(req,res)=>{
     try{
     const allPhones = await Phone.find();
     res.render('phones/all-phones.ejs',{allPhones: allPhones});
+    console.log('app is working (READ ALL PHONES)');
     }
     catch (error) {
         console.log('ERROR:', error);
@@ -14,29 +15,36 @@ router.get('/', async(req,res)=>{
 
 //create new phone
 router.get('/create', async(req,res)=>{
-    try{
-        res.render('phones/create-phone.ejs')
-    }
-    catch (error) {
-        console.log('ERROR:', error);
-    }
+    res.render('phones/create-phone.ejs');
 })
 
 router.post('/', async(req,res) => {
     try {
         if(req.body.available){
-            req.body.available = "yes";
+            req.body.available = true;
         }
         else{
-            req.body.available = "no";
+            req.body.available = false;
         }
         const createdPhone = await Phone.create(req.body);
         res.redirect('/used-phones');
+        console.log('app is working (CREATE)');
     }
     catch (error) {
         console.log('ERROR:', error);
     }
 });
 
+//delete
+router.post('/delete/:id', async (req, res) => {
+    try {
+        await Phone.findByIdAndDelete(req.params.id);
+        res.redirect('/used-phones');
+        console.log('app is working (DELETE)');
+    }
+    catch (error) {
+        console.log('ERROR:', error);
+    }
+});
 
 module.exports = router;
